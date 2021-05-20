f5440e78fd5d86dcbd4bc45d3a9079f75aaa0b4b
Josh Dorn <jr_dorn@yahoo.com>
2021-05-20 17:38:17 -0500

    Styling pie chart

--- js/main.js
+++ js/main.js
@@ -5,27 +5,36 @@ google.charts.setOnLoadCallback(drawPChart);
 function drawPChart() {
   var pData = google.visualization.arrayToDataTable([
     ["Task", "Hours per Day"],
-    ["Work", 11],
-    ["Eat", 2],
-    ["Commute", 2],
-    ["Watch TV", 2],
-    ["Sleep", 7],
+    ["Groceries", 11],
+    ["Housing", 2],
+    ["Transporation", 2],
+    ["Savings", 7],
+    ["Entertainment", 2],
   ]);
 
   var pOptions = {
-    title: "My Daily Activities",
     fontName: "'Source Sans Pro', Arial, Helvetica, sans-serif",
-    pieHole: 0.6,
-    backgroundColor: "#202124",
-    pieSliceTextStyle: {
-      color: "#e7eaed",
-    },
+    title: "My Monthly Budget",
     titleTextStyle: {
       color: "#e7eaed",
       fontName: "'Poppins', Arial, Helvetica, sans-serif",
     },
+    pieHole: 0.6,
+    backgroundColor: "#202124",
+    pieSliceText: "none",
+    // pieSliceTextStyle: {
+    //   color: "#e7eaed",
+    // },
     pieSliceBorderColor: "#202124",
-    colors: ["pink", "silver", "teal", "cyan", "magenta"],
+    colors: [
+      "#fc0100",
+      "#fc6302",
+      "#fb9f02",
+      "#1fbc84",
+      "#0405bb",
+      "#6e66f9",
+      "#883eff",
+    ],
     legend: {
       textStyle: {
         color: "#e7eaed",
